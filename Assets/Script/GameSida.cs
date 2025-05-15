bb429d5fff5dba5dd67b6e1049d31f53154c1f62
= <hotrung6935@gmail.com>
2025-05-15 21:11:27 +0700

    new game 2d by Unity

---
+++ Assets/Script/GameSida.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameSida : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Mãi yêu Trần Hoài Ngọc , yêu 4 năm rồi cái địt con mẹ");
+    }
+
+    void Update()
+    {
+        Debug.Log("Tôi là bạn trai của THN");
+        Debug.Log(Time.deltaTime.ToString());
+    }
+}
